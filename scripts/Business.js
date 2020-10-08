d7b30f407a91f6fed1c5a3beed3fb4b82453b002
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-08 12:09:52 -0500

    forEach method exercise is finsihed.

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,5 +1,11 @@
-export const business = () => {
-    return `
-    
-    `
-}
+export const business = (businessObj) => {
+  return `
+    <h2>${businessObj.companyName}</h2>
+    <br>
+    <div class="business__street">${businessObj.addressFullStreet}</div>
+    <div class="business__location">${businessObj.ddressCity}, ${businessObj.addressStateCode} ${businessObj.addressZipCode}</div>
+    <div class="seperator">
+    <br>
+    <div class="deco"></div></div> 
+    `;
+};
