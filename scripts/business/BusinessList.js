d0712610d4d821592ed715d27a39a067c790995b
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-12 14:27:46 -0500

    seach funcitonal

--- scripts/business/BusinessList.js
+++ scripts/business/BusinessList.js
@@ -2,41 +2,57 @@ import { useBusinesses, useNY, useManufacturing } from "./BusinessProvider.js";
 import { business } from "./Business.js";
 
 export const BusinessList = () => {
-  const content = document.querySelector(".main-container");
-  // Company Array
+  // Array
   const businesses = useBusinesses();
-  const nYBusinesses = useNY();
-  const manufactionBusinesses = useManufacturing();
+  // Target Node
+  const companySearchResultArticle = document.querySelector(".foundCompanies");
+  // Target Search
+  const searchInputBusiness = document.querySelector("#companySearch");
+  //  Event
+  searchInputBusiness.addEventListener("keypress", (keyPressEvent) => {
+    if (keyPressEvent.charCode === 13) {
+      const foundBusiness = businesses.find((business) =>
+        business.companyName.includes(keyPressEvent.target.value)
+      );
 
-  console.log(businesses);
-
-  let activeBusinesses = "";
-  let nYActiveBusinesses = "";
-  let manufactionActiveBusinesses = "";
-
-  businesses.forEach((company) => (activeBusinesses += business(company)));
-  nYBusinesses.forEach((company) => (nYActiveBusinesses += business(company)));
-  manufactionBusinesses.forEach((company) => (manufactionActiveBusinesses += business(company)));
-
-  return (content.innerHTML += `
-        <section class="businesses">
-                <h1>Active Businesses</h1>
-                <input type="text" placeholder="Enter business name" id="companySearch" />
-                <div class="businesses__list">
-                  ${activeBusinesses}
-                </div>
-
-                <article class="businessList--newYork">
-            <h2>New York Businesses</h2>
-            ${nYActiveBusinesses}
-            </article>
-
-            <article class="businessList--manufacturing">
-            <h2>Manufacturing Businesses</h2>
-            ${manufactionActiveBusinesses}
-            </article>
-            </section>
-
-            
-        `);
+      return (companySearchResultArticle.innerHTML += `
+      ${business(foundBusiness)}
+    `);
+    }
+  });
 };
+
+//   const nYBusinesses = useNY();
+//   const manufactionBusinesses = useManufacturing();
+
+//   console.log(businesses);
+
+//   let activeBusinesses = "";
+//   let nYActiveBusinesses = "";
+//   let manufactionActiveBusinesses = "";
+
+//   businesses.forEach((company) => (activeBusinesses += business(company)));
+//   nYBusinesses.forEach((company) => (nYActiveBusinesses += business(company)));
+//   manufactionBusinesses.forEach((company) => (manufactionActiveBusinesses += business(company)));
+
+//   return (content.innerHTML += `
+//         <section class="businesses">
+//                 <h1>Active Businesses</h1>
+//
+//                 <div class="businesses__list">
+//                   ${activeBusinesses}
+//                 </div>
+
+//                 <article class="businessList--newYork">
+//             <h2>New York Businesses</h2>
+//             ${nYActiveBusinesses}
+//             </article>
+
+//             <article class="businessList--manufacturing">
+//             <h2>Manufacturing Businesses</h2>
+//             ${manufactionActiveBusinesses}
+//             </article>
+//             </section>
+
+//         `);
+// };
